fc9c4b558ec1dd2a606f0c122ce2e6577ea9243d
Simeon Todorov <59865649+dinocom33@users.noreply.github.com>
2023-03-01 11:41:31 +0200

    Inheritance - Lab

---
+++ 3-Inheritance/1-Lab/stack_of_strings.py
@@ -0,0 +1,24 @@
+from typing import List
+
+
+class Stack:
+    def __init__(self):
+        self.data: List[str] = []
+
+    def push(self, element: str) -> None:
+        self.data.append(element)
+
+    def pop(self) -> str:
+        return self.data.pop()
+
+    def top(self) -> str:
+        return self.data[-1]
+
+    def is_empty(self) -> bool:
+        if self.data:
+            return False
+
+        return True
+
+    def __str__(self):
+        return "[" + ', '.join(reversed(self.data)) + "]"
