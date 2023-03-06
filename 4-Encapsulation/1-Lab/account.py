f5a421f290cb3259352b2985c8241b2b4271056a
Simeon Todorov <59865649+dinocom33@users.noreply.github.com>
2023-03-06 11:02:36 +0200

    Encapsulation - Lab

---
+++ 4-Encapsulation/1-Lab/account.py
@@ -0,0 +1,26 @@
+class Account:
+    def __init__(self, account_id: int, balance: float, pin:int):
+        self.__id = account_id
+        self.balance = balance
+        self.__pin = pin
+
+    def get_id(self, pin: int):
+        if not self.__pin == pin:
+            return "Wrong pin"
+
+        return self.__id
+
+    def change_pin(self, old_pin, new_pin):
+        if not self.__pin == old_pin:
+            return "Wrong pin"
+
+        self.__pin = new_pin
+        return "Pin changed"
+
+
+account = Account(8827312, 100, 3421)
+print(account.get_id(1111))
+print(account.get_id(3421))
+print(account.balance)
+print(account.change_pin(2212, 4321))
+print(account.change_pin(3421, 1234))
